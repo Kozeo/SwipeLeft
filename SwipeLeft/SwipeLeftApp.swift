27f8945b0ff14080612c555233e0c8668f697ed5
Elliot <elliot.gaubert@hotmail.com>
2025-05-07 20:52:01 +0200

    Initial Commit

---
+++ SwipeLeft/SwipeLeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeLeftApp.swift
+//  SwipeLeft
+//
+//  Created by Elliot Gaubert on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwipeLeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwipeLeftTests/SwipeLeftTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeLeftTests.swift
+//  SwipeLeftTests
+//
+//  Created by Elliot Gaubert on 07/05/2025.
+//
+
+import Testing
+@testable import SwipeLeft
+
+struct SwipeLeftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
